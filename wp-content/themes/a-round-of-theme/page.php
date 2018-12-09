88dc2492e1f60c879d1f4c7479f6ec2b759194a1
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-09 01:04:00 -0800

    added section and div class to encompass page created content

--- wp-content/themes/a-round-of-theme/page.php
+++ wp-content/themes/a-round-of-theme/page.php
@@ -1,10 +1,14 @@
 <!-- brings in header -->
 <?php get_header(); ?>
 
-<!-- pages created -->
-<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
-<?php the_content(); ?>
-<?php endwhile; endif; ?>
+<section class="page-template">
+	<div class="container">
+		<!-- pages created -->
+		<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php the_content(); ?>
+		<?php endwhile; endif; ?>
+	</div>
+</section>
 
 <!-- brings in footer -->
 <?php get_footer(); ?>
