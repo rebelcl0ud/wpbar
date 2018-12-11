ab4ce8c1e87f0d4e907c820db3d2c5299681d5c5
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-11 01:56:22 -0800

    add: support for excerpts + ref links of methods commented out top of script

--- wp-content/themes/a-round-of-theme/functions.php
+++ wp-content/themes/a-round-of-theme/functions.php
@@ -1,7 +1,9 @@
 <?php 
-	// https://developer.wordpress.org/reference/
+	// https://developer.wordpress.org/reference/functions/add_theme_support/
+	// https://developer.wordpress.org/reference/functions/add_post_type_support/
 	function post_thumbnails() {
 		add_theme_support( 'post-thumbnails' );
+		add_post_type_support( 'page', 'excerpt' );
 	}
 
 	add_action('after_setup_theme', 'post_thumbnails');
