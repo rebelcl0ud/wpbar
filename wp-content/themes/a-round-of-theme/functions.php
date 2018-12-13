aed630e6912f86c109d80cfd1bed7dd4081b0668
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-13 13:33:18 -0800

    add: sidebar/ widget functionality with ref links commented out above function

--- wp-content/themes/a-round-of-theme/functions.php
+++ wp-content/themes/a-round-of-theme/functions.php
@@ -5,7 +5,20 @@
 		add_theme_support( 'post-thumbnails' );
 		add_post_type_support( 'page', 'excerpt' );
 	}
-
 	add_action('after_setup_theme', 'post_thumbnails');
+
+	// https://developer.wordpress.org/themes/functionality/sidebars/#examples
+	// https://developer.wordpress.org/themes/functionality/sidebars/#load-your-sidebar
+	function portfolio_widgets_init() {
+		register_sidebar( array (
+			'name'          => __('right-sidebar', 'portfolio'),
+			'id'            => 'sidebar-1',
+			'before_widget' => '<div id="%1$s" class="widget-box %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3> '
+		));
+	}
+	add_action('widgets_init', 'portfolio_widgets_init');
 
 ?>
