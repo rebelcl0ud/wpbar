c80964f78d508e1a63b8d2308566c50b38ee8a16
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-13 23:33:06 -0800

    update: script tag at bottom of footer, type=module; app.js

--- wp-content/themes/a-round-of-theme/footer.php
+++ wp-content/themes/a-round-of-theme/footer.php
@@ -17,7 +17,7 @@
       <h6>Web Developer</h6>
     </footer>
 
-    <script src="<?php echo get_bloginfo('template_directory'); ?>/js/app.js"></script>
+    <script type="module" src="<?php echo get_bloginfo('template_directory'); ?>/js/app.js"></script>
 
   <?php wp_footer(); ?>
   </body>
