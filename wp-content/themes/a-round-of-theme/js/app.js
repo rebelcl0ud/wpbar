606baf5ebf2b11ba9435e1f5de4969c4ca852fbe
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-14 13:42:29 -0800

    add: testimonials section; import html, render from lit-html; axios usage to snag IDs used for 2nd req to snag images used in testimonials

--- wp-content/themes/a-round-of-theme/js/app.js
+++ wp-content/themes/a-round-of-theme/js/app.js
@@ -7,3 +7,40 @@ var clickedBtn = function() {
 menuBtn[0].addEventListener('click', clickedBtn)
 
 console.log(menuBtn[0])
+
+/* --------------- TESTIMONIALS ----------------- */
+
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+let postData = [];
+let postImages = [];
+
+// https://github.com/axios/axios
+axios.get('wp-json/wp/v2/testimonials')
+	.then((response) => {
+		// console.log(response.data)
+		postData = response.data;
+		// loop over posts, snag IDs
+		postData.map((item) => {
+			// console.log(item.id);
+
+			// IDs used to snag images
+			return axios.get(`http://localhost:8888/wp-json/wp/v2/media?parent=${item.id}`)
+				.then((res) => {
+					// console.log(res.data)
+					let imageLookup = res.data;
+					imageLookup.map((item) => {
+						postImages.push({
+							id: item.id,
+							image: item.media_details.sizes.medium.source_url
+						});
+						console.log(postImages);
+					})
+				})
+		})
+		
+		
+	})
+	.catch((err) => {
+		console.error(err)
+	})
