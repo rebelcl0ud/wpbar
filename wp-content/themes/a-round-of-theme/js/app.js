c4355c49d6edf19fdb7606eb4d606c727ba1a1ba
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-14 16:54:30 -0800

    add: brought in testimonial html from index.php, assigned to 'appTemplate' using lit-html that was imported and set up to render into 'testimonials-app' in index.php; parent function initApp is triggered after 2nd get req which has a swap method giving functionality to swap array of data from get req

--- wp-content/themes/a-round-of-theme/js/app.js
+++ wp-content/themes/a-round-of-theme/js/app.js
@@ -34,7 +34,8 @@ axios.get('wp-json/wp/v2/testimonials')
 							id: item.id,
 							image: item.media_details.sizes.medium.source_url
 						});
-						console.log(postImages);
+						// console.log(postImages);
+						initApp(response);
 					})
 				})
 				// to catch error of 2nd call
@@ -47,3 +48,48 @@ axios.get('wp-json/wp/v2/testimonials')
 	.catch((err) => {
 		console.error(err)
 	})
+
+	const initApp = (data) => {
+		let testimonialsData = data.data;
+
+		Array.prototype.swap = ((x,y) => {
+			let b = this[x];
+			this[x] = this[y];
+			this[y] = b;
+			return this;
+		})
+
+		const appTemplate = (data) => html `
+			<div class="testimonials-container">
+        <div class="test-sides test-left">
+          <div class="person-img" style="background: url('https://d3iw72m71ie81c.cloudfront.net/male-30.jpg');">
+            <div class="hover-bg">
+              <div class="name">James</div>
+            </div>
+          </div>
+        </div>
+        <div class="test-center">
+          <div class="header">
+            <div class="user-img" style="background: url('https://d3iw72m71ie81c.cloudfront.net/8912fe22-7970-4e15-a3a1-abef9f2fb4b5')">
+
+            </div>
+            <div class="info">
+              <h4>Jenny Benzino</h4>
+              <span>CEO, Nike</span>
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div class="test-sides test-right">
+          <div class="person-img" style="background: url('https://d3iw72m71ie81c.cloudfront.net/88b95197-fd1e-4e11-8793-2903a5cfd06e-10584053_10153749310922416_3125632463004974493_n.jpg')">
+            <div class="hover-bg">
+              <div class="name">Bryant</div>
+            </div>
+          </div>
+        </div>
+      </div>
+		`
+		render(appTemplate(postData), document.getElementById('testimonials-app'));
+	}
