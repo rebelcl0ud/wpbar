10207e1d6c7005669764318e9626c86b0bba653e
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-14 16:56:37 -0800

    remove: testimonials-container moved to app.js, initial setup, to use within appTemplate var on initApp trigger using lit-html that was imported in app.js

--- wp-content/themes/a-round-of-theme/index.php
+++ wp-content/themes/a-round-of-theme/index.php
@@ -206,38 +206,7 @@
         </div>
         <!-- setting up testimonial -->
         <div id="testimonials-app">
-          
-        </div>
-        
-        <div class="testimonials-container">
-          <div class="test-sides test-left">
-            <div class="person-img" style="background: url('https://d3iw72m71ie81c.cloudfront.net/male-30.jpg');">
-              <div class="hover-bg">
-                <div class="name">James</div>
-              </div>
-            </div>
-          </div>
-          <div class="test-center">
-            <div class="header">
-              <div class="user-img" style="background: url('https://d3iw72m71ie81c.cloudfront.net/8912fe22-7970-4e15-a3a1-abef9f2fb4b5')">
-
-              </div>
-              <div class="info">
-                <h4>Jenny Benzino</h4>
-                <span>CEO, Nike</span>
-              </div>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div class="test-sides test-right">
-            <div class="person-img" style="background: url('https://d3iw72m71ie81c.cloudfront.net/88b95197-fd1e-4e11-8793-2903a5cfd06e-10584053_10153749310922416_3125632463004974493_n.jpg')">
-              <div class="hover-bg">
-                <div class="name">Bryant</div>
-              </div>
-            </div>
-          </div>
+          <!-- appTemplate data from app.js will go here -->
         </div>
       </div>
     </section>
