ab4ae2a32f5ef9493dbbff2fbf6a78886609fd91
rebelcl0ud <jovanka.placeres@gmail.com>
2018-12-17 18:03:02 -0800

    update: pod added within wordpress with fields holding info hard coded within script initially; brings in same info previously displayed but through the use of pods in wordpress dashboard

--- wp-content/themes/a-round-of-theme/footer.php
+++ wp-content/themes/a-round-of-theme/footer.php
@@ -1,20 +1,38 @@
- <footer>
+    <footer>
       <div class="social-container">
-        <a href="http://www.facebook.com/">
-          <i class="fab fa-facebook"></i>
-        </a>
-        <a href="http://www.dribbble.com/">
-          <i class="fab fa-dribbble"></i>
-        </a>
-        <a href="http://www.facebook.com/">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="http://www.facebook.com/">
-          <i class="fab fa-instagram"></i>
-        </a>
+        <!-- vars assigned -->
+        <?php 
+          $mypod = pods('social_link');
+          $mypod->find('name');
+        ?>
+        <!-- below loops over social stuff created -->
+        <?php while ($mypod->fetch()) : ?>
+          <?php 
+            // set vars
+            $name = $mypod->field('name');
+            $content = $mypod->field('content');
+            $facebook_link = $mypod->field('facebook_link');
+            $dribbble_link = $mypod->field('dribbble_link');
+            $twitter_link = $mypod->field('twitter_link');
+            $instagram_link = $mypod->field('instagram_link');
+          ?>
+          <a href="<?php echo $facebook_link ?>">
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="<?php echo $dribbble_link ?>">
+            <i class="fab fa-dribbble"></i>
+          </a>
+          <a href="<?php echo $twitter_link ?>">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="<?php echo $instagram_link ?>">
+            <i class="fab fa-instagram"></i>
+          </a>
       </div>
-      <h5>Joe Santos Garcia</h5>
-      <h6>Web Developer</h6>
+      <h5><?php echo $name ?></h5>
+      <h6><?php echo $content ?></h6>
+      <?php endwhile; ?>
+      <!-- end loop over social stuff created -->
     </footer>
 
     <script type="module" src="<?php echo get_bloginfo('template_directory'); ?>/js/app.js"></script>
